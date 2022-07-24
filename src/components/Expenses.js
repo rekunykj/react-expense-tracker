9cefeff98be977e232db6f5eded9fa06da3fa3c2
jrekunyk <jrekunyk@mykey.com>
2022-07-23 18:47:23 -0600

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <div className="expenses">
+      {expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Expenses;
